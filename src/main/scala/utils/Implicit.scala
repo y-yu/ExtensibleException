696971bfe68fe15a4cd335900f13e4347e7b0735
Yoshimura Yuu <m@yyu.pw>
2015-08-16 12:18:40 +0900

    fix

--- src/main/scala/utils/Implicit.scala
+++ src/main/scala/utils/Implicit.scala
@@ -2,17 +2,17 @@ package utils
 
 object Implicit {
   implicit class ExceptionEither[L <: RootException, R](val ee: Either[L, R]) {
-    def map[L2 >: L <: RootException, R2](f: R => R2)(implicit L2: L ~> L2): Either[L2, R2] = ee match {
+    def map[L2 <: RootException, R2](f: R => R2)(implicit L2: L ~> L2): Either[L2, R2] = ee match {
       case Left(e)  => Left(L2.cast(e))
       case Right(v) => Right(f(v))
     }
 
-    def flatMap[L2, R2](f: R => Either[L2, R2])(implicit L2: L ~> L2): Either[L2, R2] = ee match {
+    def flatMap[L2 <: RootException, R2](f: R => Either[L2, R2])(implicit L2: L ~> L2): Either[L2, R2] = ee match {
       case Left(e)  => Left(L2.cast(e))
       case Right(v) => f(v)
     }
 
-    def as[L2](implicit L2: L ~> L2): Either[L2, R] = ee match {
+    def as[L2 <: RootException](implicit L2: L ~> L2): Either[L2, R] = ee match {
       case Left(e)  => Left(L2.cast(e))
       case Right(v) => Right(v)
     }

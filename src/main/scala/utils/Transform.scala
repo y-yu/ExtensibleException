3d10f37077d7bf60951fea5e527dcc2388051ce6
matsu-chara <matsuy00@gmail.com>
2015-09-08 21:48:07 +0900

    macroを使ったTransformインスタンスの定義

--- src/main/scala/utils/Transform.scala
+++ src/main/scala/utils/Transform.scala
@@ -1,4 +1,6 @@
 package utils
+import scala.language.experimental.macros
+import scala.reflect.macros.whitebox.Context
 
 trait :->[-A, +B] {
   def cast(a: A): B
@@ -7,6 +9,21 @@ trait :->[-A, +B] {
 object :-> {
   implicit def superclass[A, B >: A] = new :->[A, B] {
     def cast(a: A): B = a
+  }
+}
+
+object Transform {
+  def castable[A, B]: A :-> B = macro castableImpl[A, B]
+
+  def castableImpl[A: c.WeakTypeTag, B: c.WeakTypeTag](c: Context): c.Expr[A :-> B] = {
+    import c.universe._
+
+    val fromClassSym = c.weakTypeOf[A].typeSymbol
+    val toClassSym = c.weakTypeOf[B].typeSymbol
+
+    c.Expr[A :-> B](q"""new :->[$fromClassSym, $toClassSym] {
+      def cast(a: $fromClassSym): $toClassSym = ${toClassSym.companion}.apply(a)
+      }""")
   }
 }
 

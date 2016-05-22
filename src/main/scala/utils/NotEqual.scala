260173e68830663ecdd1ac506cdca86d2d3b84b6
Yoshimura Yuu <m@yyu.pw>
2016-05-22 21:36:59 +0900

    remove NotEqual

--- src/main/scala/utils/NotEqual.scala
+++
@@ -1,9 +0,0 @@
-package utils
-
-trait <:!<[A, B]
-
-object <:!< {
-  implicit def nsub[A, B]: A <:!< B = new <:!<[A, B] {}
-  implicit def nsubAmbig1[A, B >: A]: A <:!< B = sys.error("Unexpected call")
-  implicit def nsubAmbig2[A, B >: A]: A <:!< B = sys.error("Unexpected call")
-}
